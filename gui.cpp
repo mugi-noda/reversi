99069aec94854d541a063cc62783e9fa23c62a5e
mugi-noda <34913302+mugi-noda@users.noreply.github.com>
2018-01-17 02:19:45 +0900

    Add files via upload

---
+++ gui.cpp
@@ -0,0 +1,48 @@
+//g++ gui.cpp -std++11 -O3 -mtune=native -march=native -flto
+#include "Game/Game.hpp"
+
+void sente(){
+	Game game;
+	game.show();
+	while(!game.isGameover()){
+		if(game.isGameover()){
+			break;
+		}
+		game.playerMove();
+		game.show();
+		if(game.isGameover()){
+			break;
+		}
+		game.aiMove();
+		game.show();
+	}
+}
+
+void gote(){
+	Game game;
+	game.show();
+	while(!game.isGameover()){
+		if(game.isGameover()){
+			break;
+		}
+		game.aiMove();
+		game.show();
+		if(game.isGameover()){
+			break;
+		}
+		game.playerMove();
+		game.show();
+	}
+}
+
+int main(){
+	std::cout << "sente:1, gote:0" << '\n';
+	int hoge = 0;
+	std::cin >> hoge;
+	if (hoge == 0){
+		gote();
+	}else	if(hoge == 1){
+		sente();
+	}
+	return 0;
+}
